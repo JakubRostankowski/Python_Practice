704916a7acdbf4afff69f34c8ae9fcf147946146
Jakubsoner <42152713+Jakubsoner@users.noreply.github.com>
2018-12-31 15:27:34 +0100

    Update calc2.py

--- calc2.py
+++ calc2.py
@@ -1,10 +1,11 @@
 while True:
-	n1 = int(raw_input("Show numbers from: "))
-	n2 = int(raw_input("to: "))
-	y = int(raw_input("divisible by: "))
-	for x in range(n1, (n2+1)):
-    		if x % y == 0:
-        		print x,
-        q = raw_input("\nDo you want to check for new numbers (yes/no)?")
-        if len(q) <=2:
-          	break
+    n1 = int(input("Show numbers from: "))
+    n2 = int(input("to: "))
+    y = int(input("divisible by: "))
+    for x in range(n1, (n2+1)):
+        if x % y == 0:
+            print (x),
+    q = input("\nDo you want to check for new numbers (yes/no)?")
+    if len(q) <=2:
+        break
+
