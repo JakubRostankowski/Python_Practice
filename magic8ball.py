21a2aabcc30ee5b51d23d701f374bf3042601c23
Jakub Rostankowski <42152713+JakubRostankowski@users.noreply.github.com>
2019-01-12 12:27:13 +0100

    Add files via upload

---
+++ magic8ball.py
@@ -0,0 +1,23 @@
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'To pewne'
+    elif answerNumber == 2:
+        return 'Zdecydowanie tak'
+    elif answerNumber == 3:
+        return 'Tak'
+    elif answerNumber == 4:
+        return 'Niejasna odpowiedź'
+    elif answerNumber == 5:
+        return 'Zapytaj później'
+    elif answerNumber == 6:
+        return 'Nie'
+    elif answerNumber == 7:
+        return 'Być może'
+    elif answerNumber == 8:
+        return 'Nie licz na to'
+
+r = random.randint(1,8)
+fortune = getAnswer(r)
+print(fortune)
